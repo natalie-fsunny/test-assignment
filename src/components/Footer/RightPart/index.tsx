65c47b068d5ff89e964843b4a4b9cb3c4b389cea
natalie-fsunny <natalie.fsunny@gmail.com>
2022-08-24 15:50:28 +0400

    fixed form in footer

--- src/components/Footer/RightPart/index.tsx
+++ src/components/Footer/RightPart/index.tsx
@@ -11,13 +11,13 @@ export const RightPart = () => {
       <h4 className={styles.subtitle}>{t("footer.subscribe_to_our_blog")}</h4>
       <div className={styles.slogan}>{t("footer.slogan")}</div>
       <section className={styles.subscribtion_form}>
-        <div className={styles.form}>
-          <Input placeholder="E-mail" />
-          <Button>
+        <form className={styles.form}>
+          <Input placeholder="E-mail" type="email"/>
+          <Button type="submit">
             {t("footer.subscribe")}
             <Icon variant="arrowRightFilled" fontSize={15} />
           </Button>
-        </div>
+        </form>
         <div className={styles.description}>{t("footer.description")}</div>
       </section>
       <div className={styles.social_icons}>
